7320215590368c6ba97fbace75fceb9ebb08f467
Danny Konstadt <dkonst001@users.noreply.github.com>
2016-10-16 08:20:29 +0300

    Add files via upload

---
+++ targilim_6_2/targilim_6_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace targilim_6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            RectanglePrism rectPri = new RectanglePrism();
+            rectPri.Hight = rand.Next();
+            rectPri.Width = rand.Next();
+            rectPri.Length = rand.Next();
+            Console.WriteLine("Length {0}, Width {1}, Hight {2}, Area {3}, Volume {4}",
+                rectPri.Length, rectPri.Width, rectPri.Hight,rectPri.PrismArea(), rectPri.PrismVolume());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ targilim_6_2/targilim_6_2/RectanglePrism.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace targilim_6_2
+{
+    class RectanglePrism
+    {
+        long _hight;
+        long _width;
+        long _length;
+
+        public long Hight
+        {
+            get
+            {
+                return _hight;
+            }
+
+            set
+            {
+                _hight = value;
+            }
+        }
+
+        public long Width
+        {
+            get
+            {
+                return _width;
+            }
+
+            set
+            {
+                _width = value;
+            }
+        }
+
+        public long Length
+        {
+            get
+            {
+                return _length;
+            }
+
+            set
+            {
+                _length = value;
+            }
+        }
+
+        public RectanglePrism()
+        {
+            Hight = 0;
+            Width = 0;
+            Length = 0;
+        }
+
+        public long PrismVolume()
+        {
+            return Hight*Length*Width;
+        }
+
+        public long PrismArea()
+        {
+            return 2*Hight*Length+2*Hight*Width+2*Width*Length;
+        }
+    }
+}
