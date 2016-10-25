a0f7acd281f7f1676e06027a3206bb1b80466159
Danny Konstadt <dkonst001@users.noreply.github.com>
2016-10-25 11:36:53 +0300

    Add files via upload

---
+++ fibonacci-recursion/fibonacci-recursion/FibRec.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacci_recursion
+{
+    public static class FibRec
+    {
+        public static int Fib(int n)
+        {
+            if (n == 1) return n;
+            if (n == 2) return n;
+
+            return Fib(n - 1) + Fib(n - 2);
+
+        }
+    }
+}
---
+++ fibonacci-recursion/fibonacci-recursion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacci_recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //a(n) = a(n-1) + a(n-2)
+
+            Console.WriteLine(FibRec.Fib(6));
+            Console.ReadLine();
+        }
+    }
+}
