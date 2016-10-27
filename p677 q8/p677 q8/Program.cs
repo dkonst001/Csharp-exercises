c04d8d5480d4a7d98581ce139e28b34387622e51
Danny Konstadt <dkonst001@users.noreply.github.com>
2016-10-27 11:00:16 +0300

    Add files via upload

---
+++ p677 q8/p677 q8/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p677_q8
+{
+    class Program
+    {
+        //The majorantof  an array  of size  N  is  a value  that occurs  in  it at  
+        //least N/2 + 1 times.Write a program that finds the majorantof given array 
+        //and prints it.If it does not exist, print "The majorant does not exist!".
+        //Example: { 2, 2, 3, 3, 2, 3, 4, 3, 3} 3
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 2, 2, 3, 3, 2, 3, 2,2, 3 };
+
+            List<int> majorant = list.FindAll(i => (list.Count(i1 => i1 == i) == ((list.Count() / 2) + 1)));
+
+            IEnumerable<int> li = majorant.Distinct();
+
+            if (li.Count() > 0) PrintList("The majorant is", li.ToList());
+            else Console.WriteLine("The majorant does not exist!");
+
+            Console.ReadLine();
+            
+            //IGrouping<int, int> li = majorant.GroupBy(i=>i);
+        }
+
+        private static void PrintList(string state, List<int> list)
+        {
+            string str = "";
+            foreach (int i in list)
+            {
+                str += i + ",";
+            }
+            Console.WriteLine(state + ":" + str.Remove(str.Length-1,1));
+        }
+    }
+}
---
+++ p677 q9/p677 q9/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p677_q9
+{
+    class Program
+    {
+        //We are given the following sequence:
+        //S1= N;
+        //S2= S1 + 1;
+        //S3= 2*S1 + 1;
+        //S4= S1 + 2;
+        //S5= S2 + 1;
+        //S6= 2*S2 + 1;
+        //S7= S2 + 2;
+        //...
+        //Using the Queue<T> class, write a program which by given N prints on the console 
+        //the first 50 elements of the sequence.
+        //Example: N= 2 
+        //2, 3, 5, 4, 4, 7, 5, 6, 11, 7, 5, 9, 6, ...
+        static void Main(string[] args)
+        {
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(2);
+            List<int> S = new List<int>();
+
+
+            while (S.Count() < 50)
+            {
+                int current = queue.Dequeue();
+                S.Add(current);
+
+                queue.Enqueue(current + 1);
+                queue.Enqueue(2 * current + 1);
+                queue.Enqueue(current + 2);
+            }
+
+            PrintList("S=", S);
+            Console.ReadLine();
+        }
+        private static void PrintList(string state, List<int> list)
+        {
+            string str = "";
+            foreach (int i in list)
+            {
+                str += i + ",";
+            }
+            Console.WriteLine(state + ":" + str.Remove(str.Length - 1, 1));
+        }
+    }
+}
